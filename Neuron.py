1cf2ef37d4c4a371d5fdd1dfe8f7e65b17cce38a
heyitsraman <94373661+heyitsraman@users.noreply.github.com>
2021-11-15 19:28:06 +0530

    Update Neuron.py

--- Neuron.py
+++ Neuron.py
@@ -1,9 +1,11 @@
+# Creating Artificial Neuron with Sigmoid Activation Function
+
 import math
-def sigmoid(x):
+def sigmoid(x):         #defining activation function Sigmoid    
     y = 1.0 / (1+ math.exp(-x))
     return y
 
-def activate(inputs, weights):
+def activate(inputs, weights):      #defining neuron calculations
     #  perform summation
         h=0
         for x,w in zip(inputs,weights):
@@ -11,8 +13,8 @@ def activate(inputs, weights):
     # perform activation
         return sigmoid(h)
 
-inputs = [0.5, 0.3, 0.2]
-weights = [0.4, 0.7, 0.2]
+inputs = [0.32, 0.3, 0.2]
+weights = [0.4, 0.45, 0.2]
 output = activate(inputs, weights)
 
-print(output)
+print(output)       #artificial neuron generated
